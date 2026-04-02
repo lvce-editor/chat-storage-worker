6569e2a1b59262016ba26776cb3b5eeb0f097284
Le Vivilet <levivilet@pm.me>
2026-04-02 07:58:20 +0200

    feature: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,6 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
+import * as regex from '@lvce-editor/eslint-plugin-regex'
 
-export default [
-  ...config.default,
-  ...actions.default,
-  ...tsconfig.default,
-  {
-    rules: {
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-    },
-  },
-]
+export default [...config.default, ...actions.default, ...tsconfig.default, ...regex.default]
